7434dc642f6ac12b48777d1bc45a832335059be9
annrove <annrove@gmail.com>
2022-04-21 18:09:54 -0500

    Cambio en Formulario Index y Login

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ app.listen(3000, () => {
     console.log("Servidor Funcionando");
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/login.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/productCart.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/productDetail.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + './views/productDetail.html');
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/register.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
 });
