bb8fa2161566043b7a60c9e0aae4b73080b14a44
david <guillermo01.galp@gmail.com>
2022-05-10 18:01:02 -0500

    Sprint 3

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,12 +2,6 @@ const controller = {
   productDetail: (req, res) => {
     res.render("./products/productDetail");
   },
-  addProduct: (req, res) => {
-    res.render("./products/addProduct");
-  },
-  editProduct: (req, res) => {
-    res.render("./products/editProduct");
-  },
 };
 
 module.exports = controller;
--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/productsController");
 
-router.get("/detail", controller.productDetail);
-router.get("/add", controller.addProduct);
-router.get("/edit", controller.editProduct);
+router.get("/product-detail", controller.productDetail);
 
 module.exports = router;
